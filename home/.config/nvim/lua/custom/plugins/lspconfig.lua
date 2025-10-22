8a299f2e07d1ec7b4a44f88655dbb48d6ce31112
Mark Tilton <mark.tilton.a@gmail.com>
2025-10-21 20:02:44 -0400

    Add go and odin

--- home/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ home/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -178,7 +178,7 @@ return {
       },
       omnisharp = {},
       -- clangd = {},
-      -- gopls = {},
+      gopls = {},
       rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
