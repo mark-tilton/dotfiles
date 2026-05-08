70a5d0a32fe12da2a14e63be12797f8234c6bc0f
Mark Tilton <mark.tilton.a@gmail.com>
2026-05-08 00:09:46 -0400

    Automatically set up new tmux sessions

---
+++ home/.config/tmux/new-session-layout.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+session="$1"
+
+count=$(tmux list-windows -t "$session" | wc -l | tr -d ' ')
+[ "$count" -gt 1 ] && exit 0
+
+first=$(tmux list-windows -t "$session" -F '#{window_index}' | head -n1)
+
+tmux rename-window -t "$session:$first" nvim
+tmux send-keys     -t "$session:$first" 'nvim .' C-m
+
+tmux new-window   -t "$session:" -n term
+tmux split-window -h -t "$session:term"
+
+right=$(tmux list-panes -t "$session:term" -F '#{pane_index}' | tail -n1)
+left=$(tmux list-panes  -t "$session:term" -F '#{pane_index}' | head -n1)
+
+tmux send-keys   -t "$session:term.$right" 'claude' C-m
+tmux select-pane -t "$session:term.$left"
+
+tmux select-window -t "$session:nvim"
