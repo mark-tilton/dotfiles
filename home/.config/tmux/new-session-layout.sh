b022d49d5be8708cc24d9739e72823e46900bbba
Mark Tilton <mark.tilton.a@gmail.com>
2026-05-08 14:02:28 -0400

    Add ghostty

--- home/.config/tmux/new-session-layout.sh
+++ home/.config/tmux/new-session-layout.sh
@@ -5,9 +5,11 @@ count=$(tmux list-windows -t "$session" | wc -l | tr -d ' ')
 [ "$count" -gt 1 ] && exit 0
 
 first=$(tmux list-windows -t "$session" -F '#{window_index}' | head -n1)
+session_path=$(tmux display-message -p -t "$session:$first" '#{pane_current_path}')
+git -C "$session_path" rev-parse --is-inside-work-tree >/dev/null 2>&1 || exit 0
 
 tmux rename-window -t "$session:$first" nvim
-tmux send-keys     -t "$session:$first" 'nvim .' C-m
+tmux send-keys     -t "$session:$first" 'nvim' C-m
 
 tmux new-window   -t "$session:" -n term
 tmux split-window -h -t "$session:term"
