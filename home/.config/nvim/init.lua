a3e34b96544f5460999dc05ab29f2bf04046e4c4
Mark Tilton <mark.tilton.a@gmail.com>
2026-05-12 20:14:08 -0400

    Improve markdown and update karabiner

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -166,6 +166,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  group = vim.api.nvim_create_augroup("markdown-indent", { clear = true }),
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- home/.config/nvim/lua/custom/plugins/sleuth.lua
+++ home/.config/nvim/lua/custom/plugins/sleuth.lua
@@ -1,1 +1,7 @@
-return { "tpope/vim-sleuth" }
+return {
+  "tpope/vim-sleuth",
+  init = function()
+    -- Markdown's list-continuation indents fool the heuristic; skip detection.
+    vim.g.sleuth_markdown_heuristics = 0
+  end,
+}
