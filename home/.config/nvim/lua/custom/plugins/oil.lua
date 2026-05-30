62dea3af96ef3301424b2eb23fa455fdd20af0c7
Mark Tilton <mark.tilton.a@gmail.com>
2026-05-30 10:06:50 -0400

    Let C-h/C-l navigate through oil buffers

--- home/.config/nvim/lua/custom/plugins/oil.lua
+++ home/.config/nvim/lua/custom/plugins/oil.lua
@@ -4,6 +4,11 @@ return {
   lazy = false,
   opts = {
     view_options = { show_hidden = true },
+    -- Let C-h/C-l fall through to vim-tmux-navigator instead of oil's defaults
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+    },
   },
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "Open parent directory (Oil)" },
