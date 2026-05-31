d211ce6b1cccbe31e8a4d0ea0bd96d4302eb6adc
Mark Tilton <mark.tilton.a@gmail.com>
2026-05-30 20:17:44 -0400

    Replace nvim-cmp and LuaSnip with blink.cmp

--- home/.config/nvim/lua/custom/plugins/autocompletion.lua
+++ home/.config/nvim/lua/custom/plugins/autocompletion.lua
@@ -1,104 +1,46 @@
 -- Autocompletion
 return {
-  "hrsh7th/nvim-cmp",
+  "saghen/blink.cmp",
   event = "InsertEnter",
+  version = "1.*", -- use the prebuilt release binary (no rust toolchain needed)
   dependencies = {
-    "saadparwaiz1/cmp_luasnip",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
+    "folke/lazydev.nvim",
   },
-  config = function()
-    -- See `:help cmp`
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    luasnip.config.setup({})
+  opts = {
+    -- See `:help blink-cmp-config-keymap`. Ported from the old nvim-cmp maps.
+    keymap = {
+      preset = "none",
+      ["<C-e>"] = { "select_next", "fallback" },
+      ["<C-b>"] = { "select_prev", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      ["<Tab>"] = { "accept", "fallback" },
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      -- Jump through LSP snippet placeholders (e.g. function arguments).
+      ["<C-l>"] = { "snippet_forward", "fallback" },
+      ["<C-h>"] = { "snippet_backward", "fallback" },
+    },
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
+    completion = {
+      list = { selection = { preselect = true, auto_insert = false } },
+      menu = { border = "rounded" },
+      documentation = {
+        auto_show = true,
+        window = { border = "rounded" },
       },
-      completion = { completeopt = "menu,menuone,noinsert" },
+      ghost_text = { enabled = true },
+    },
 
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      mapping = cmp.mapping.preset.insert({
-        -- Select the [n]ext item
-        ["<C-e>"] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ["<C-b>"] = cmp.mapping.select_prev_item(),
-
-        -- Scroll the documentation window [b]ack / [f]orward
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
-        --['<CR>'] = cmp.mapping.confirm { select = true },
-        --['<Tab>'] = cmp.mapping.select_next_item(),
-        --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
-        ["<C-Space>"] = cmp.mapping.complete({}),
-
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      }),
-      experimental = {
-        ghost_text = true,
-      },
-      window = {
-        completion = { -- rounded border; thin-style scrollbar
-          border = 'rounded',
-          scrollbar = true,
-        },
-        documentation = { -- no border; native-style scrollbar
-          border = 'rounded',
-          scrollbar = true,
+    sources = {
+      default = { "lsp", "path", "lazydev" },
+      per_filetype = { markdown = { "lsp", "path" } },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100, -- show lazydev completions above LSP
         },
       },
-      -- other options
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-      },
-    })
-
-    cmp.setup.filetype("markdown", {
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "path" },
-      },
-    })
-  end,
+    },
+  },
 }
